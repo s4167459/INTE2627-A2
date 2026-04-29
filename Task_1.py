70bb6e337777d5cc0fe1945f3246cd6c80f71708
S4092661 <S4092661@student.rmit.edu.au>
2026-04-29 16:33:35 +1000

    test functions now work

--- Task_1.py
+++ Task_1.py
@@ -11,14 +11,20 @@ def read_file(filename):
     return file_contents
 
 def write_to_file(file_name, contents):
-    file = open(file_name, mode="w")
+    file = open(file_name, mode="a")
     file.write(str(contents))
+    file.close()
+
+def clear_file(file_name):
+    file = open(file_name, mode="w")
+    file.write("")
     file.close()
 
  # test to see if file is successfully created, and then see what happens when csv is output through print()
 def user_input_file_test():
     user_in = str(input("Enter CSV record: \n"))
     print("file has been overwritten with singular record for test purposes)")
+    user_in += "\n"
     return user_in
 
  # Main Code Implementation
