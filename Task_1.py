a16992d6ab06857d50765103a7fb346c0ae876e4
S4092661 <S4092661@student.rmit.edu.au>
2026-04-29 16:26:45 +1000

    test functions now work

--- Task_1.py
+++ Task_1.py
@@ -4,15 +4,15 @@ from fileinput import filename
 # Functions defined before main code implementation
 
 def read_file(filename):
-    file = filename().open()
+    file = open(filename)
     file_contents = []
     file_contents = file.read()
-    filename.close()
+    file.close()
     return file_contents
 
 def write_to_file(file_name, contents):
-    file = file_name.open()
-    file.write(contents)
+    file = open(file_name, mode="w")
+    file.write(str(contents))
     file.close()
 
  # test to see if file is successfully created, and then see what happens when csv is output through print()
@@ -22,5 +22,6 @@ def user_input_file_test():
     return user_in
 
  # Main Code Implementation
+write_to_file(r"test_warehouse.csv", user_input_file_test())
 
-write_to_file("test_warehouse.csv", user_input_file_test())
+print(read_file(r"test_warehouse.csv"))
