63a6b1f2ffbef303c563db11004377a9da4d4c10
WillBRMIT <s4167459@student.rmit.edu.au>
2026-04-29 17:14:01 +1000

    Hardcoded Cryptographic parameters and derived N, o(n) and d from provided values

--- Task_1.py
+++ Task_1.py
@@ -27,6 +27,44 @@ def user_input_file_test():
     user_in += "\n"
     return user_in
 
+# Initialise the cryptographic parameters from key doc and derived from such
+
+#Inv A
+Ap = 1210613765735147311106936311866593978079938707
+Aq = 1247842850282035753615951347964437248190231863
+Ae = 815459040813953176289801
+#Derived Inv A values
+An = Ap * Aq
+Ao_n = (Ap - 1) * (Aq - 1)
+Ad = pow(Ae, -1, Ao_n)
+
+#Inv B
+Bp = 787435686772982288169641922308628444877260947
+Bq = 1325305233886096053310340418467385397239375379
+Be = 692450682143089563609787
+#Derived Inv B values
+Bn = Bp * Bq
+Bo_n = (Bp - 1) * (Bq - 1)
+Bd = pow(Be, -1, Bo_n)
+
+#Inv C
+Cp = 1014247300991039444864201518275018240361205111 #unfortunate naming scheme
+Cq = 904030450302158058469475048755214591704639633
+Ce = 1158749422015035388438057
+#Derived Inv C values
+Cn = Cp * Cq
+Co_n = (Cp - 1) * (Cq - 1)
+Cd = pow(Ce, -1, Co_n)
+
+#Inv D
+Dp = 1287737200891425621338551020762858710281638317
+Dq = 1330909125725073469794953234151525201084537607
+De = 33981230465225879849295979
+#Derived Inv D values
+Dn = Dp * Dq
+Do_n = (Dp - 1) * (Dq - 1)
+Dd = pow(De, -1, Do_n)
+
  # Main Code Implementation
 
 add_record(r"test_warehouse.csv", user_input_file_test())
